a903ca24f9fbb12bcffd52be85f16c0c1adcde99
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-08-16 20:37:36 +0900

    ✨ feat: 할 일 추가 및 완료 상태 변경 기능 추가

---
+++ src/api/todoApi.js
@@ -0,0 +1,16 @@
+import { accessInstance } from "./axiosInstance";
+
+export const postTodo = async (data) => {
+  const response = await accessInstance.post(`/todos`, data);
+  return response.data;
+};
+
+export const getTodo = async () => {
+  const response = await accessInstance.get(`/todos`);
+  return response.data;
+};
+
+export const putTodo = async (id, newData) => {
+  const response = await accessInstance.put(`/todos/${id}`, newData);
+  return response.data;
+};
--- src/pages/Todo/TodoPage.jsx
+++ src/pages/Todo/TodoPage.jsx
@@ -1,5 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getTodo, postTodo, putTodo } from "../../api/todoApi";
 
 export default function TodoPage() {
-  return <div>TodoPage</div>;
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await getTodo();
+      setTodos(response);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleChangeTodo = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleSubmitTodo = async (e) => {
+    e.preventDefault();
+
+    try {
+      await postTodo({ todo: newTodo });
+      await fetchData();
+      setNewTodo("");
+    } catch (error) {
+      console.error("Error post data:", error);
+    }
+  };
+
+  const handleClickCheckBox = async (e) => {
+    const { id, value, checked } = e.target;
+
+    await putTodo(id, { todo: value, isCompleted: checked });
+    await fetchData();
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={handleSubmitTodo}>
+        <input
+          type="text"
+          data-testid="new-todo-input"
+          value={newTodo}
+          onChange={handleChangeTodo}
+        />
+        <button data-testid="new-todo-add-button">추가</button>
+      </form>
+
+      <br />
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                id={todo.id}
+                checked={todo.isCompleted}
+                value={todo.todo}
+                onClick={handleClickCheckBox}
+              />
+              <span>{todo.todo}</span>
+              <button type="button" data-testid="modify-button">
+                수정
+              </button>
+              <button type="button" data-testid="delete-button">
+                삭제
+              </button>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
